0cb11385b6a377e1c9704ba21d0a382d6817e42b
irwin felicien <ifelicien@gmail.com>
2021-08-09 11:20:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+//pythagorean theorem: a(sq) + b(sq) = c(sq)
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+    class purchase{
+        private double itemCost() {
+            Scanner input1 = new Scanner(System.in);
+
+            System.out.println("Please enter purchase amount");
+            double price = input1.nextDouble();
+
+            return price;
+        }
+    }
+
+class payment{
+    private double customerPMT() {
+        Scanner input2 = new Scanner(System.in);
+
+        System.out.println("Enter the amount paid");
+        double paymentIn = input2.nextDouble();
+
+        return paymentIn;
+    }
+
+}
+    class dispense {
+    private double changeMade(){
+
+
+    }
+}
